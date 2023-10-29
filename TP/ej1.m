489982c092395ce84e7ad2ed52b57c2bd8638d56
santi-nihany <santi.nihany@gmail.com>
2023-10-29 12:45:39 -0300

    bla

--- TP/ej1.m
+++ TP/ej1.m
@@ -1,8 +1,12 @@
-clear all;close all; clc; % Al comenzar un script...
-addpath('../funciones') % Para incluir los .m de la carpeta funciones.
-dt = 1e-2; % paso temporal
-t = -1:dt:1; % vector tiempo
-x = cajon(t); % funci´on caj´on definida en "cajon.m", dentro
-% de la carpeta "funciones"
-% gr´afico
-PlotCompleto([-1 1 -0.5 1.5],'t','Amplitud','Gr´afico de la SVIC',25,'m*-',1.5,t,x);
+clear all;close all; clc;
+addpath('../funciones')
+addpath('../data')
+numAl= 030123; % entrada de senial
+[x,n] = senial(numAl);
+% gráfico
+% PlotCompleto([-1 1 -0.5 1.5],'t','Amplitud','Gr´afico de la SVIC',25,'m*-',1.5,t,x);
+stem(n,x)
+title('Señal')
+xlabel('Instantes (n)')
+ylabel('Amplitud (x)')
+grid on
---
+++ funciones/TFTD.m
@@ -0,0 +1,8 @@
+function X = TFTD(x)
+    K = 10; n = [-K:K];
+    N = 3; x = cajN(n,N);
+    ds = 0.001; s = [-2:ds:2]; X = zeros(size(s));
+    for k = 1:length(s)
+        X(k)=sum(x.*exp(-1i*2*pi*s(k)*n));
+    end
+end
